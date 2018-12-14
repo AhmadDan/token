5b14a0f26fc6d9fdc4fce468ccdb3eae308aee99
AhmadDan <45672879+AhmadDan@users.noreply.github.com>
2018-12-14 19:47:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
-
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(,)
+  console.log('======================================')
+  console.log(`Logged in as ${client.user.tag}!`);
   console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
   console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log([Start] ${new Date()});
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(Logged in as * [ " ${client.user.username} " ]);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(servers! [ " ${client.guilds.size} " ]);
-  console.log(Users! [ " ${client.users.size} " ]);
-  console.log(channels! [ " ${client.channels.size} " ]);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('=======================================')
 });
 
-
-
-
-
-
-
-
-
-client.login(process.env.USER_TOKEN);
+client.login('NTIzMTUyMjE3NTQxMTgxNDQw.DvVXeQ.qgULeGA2gpvL45TaC0d7oQJRHWQ');
