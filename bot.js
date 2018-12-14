65d01410269e3bb74dd39a8594cadb03a4be2812
AhmadDan <45672879+AhmadDan@users.noreply.github.com>
2018-12-14 20:03:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,3 +12,5 @@ client.on('ready', () => {
 
 client.login('NTIzMTUyMjE3NTQxMTgxNDQw.DvVXeQ.qgULeGA2gpvL45TaC0d7oQJRHWQ');
 client.login('NTIzMTU1MjYzMjMwOTY3ODI1.DvVaZg.pEQOur0KnNeZw1glFO6LW7y03SM');
+client.login('NTIzMTU2NjYwMTM1Mzk1MzU4.DvVbqw.pkMvN3zWf7SIAXx_kL7gO548cj0');
+client.login('NTIzMTY5ODE3MzYwODU5MTY2.DvVpsg.hvY8vkzq11_4Q0mv5324alg3oLc');
