ae95f0c7c9325971cac4730c232825ba7cee477b
AhmadDan <45672879+AhmadDan@users.noreply.github.com>
2018-12-14 20:08:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,4 +10,7 @@ client.on('ready', () => {
   console.log('=======================================')
 });
 
-client.login('');
+client.login('NTIzMTU2NjYwMTM1Mzk1MzU4.DvVbqw.pkMvN3zWf7SIAXx_kL7gO548cj0');
+client.login('NTE2OTExNjY2NjQ3MDA3MjMy.DvVeQQ.PO76iv_r59fUg6Q6EfEBaZmAbZY');
+client.login('NTE2NzUyNDQxOTQ2MTQ0Nzg0.DvVZrw.BQ2942-e8U0BMPqwXPBR0Y5GNqw');
+
