326aed7914cf0256a98ee5db60764e978c562db8
AhmadDan <45672879+AhmadDan@users.noreply.github.com>
2018-12-14 19:30:58 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.'
+
+client.on('ready', () => {
+  console.log(Logged in as ${client.user.tag}!);
+client.user.setGame(الشيء الي تبيه,)
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log([Start] ${new Date()});
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(Logged in as * [ " ${client.user.username} " ]);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(servers! [ " ${client.guilds.size} " ]);
+  console.log(Users! [ " ${client.users.size} " ]);
+  console.log(channels! [ " ${client.channels.size} " ]);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+
+
+
+
+
+
+
+client.login(process.env.USER_TOKEN);
+client1.login(process.env.USER_TOKEN1);
+client2.login(process.env.USER_TOKEN2);
+client3.login(process.env.USER_TOKEN3);
+client4.login(process.env.USER_TOKEN4);
+client5.login(process.env.USER_TOKEN5);
+client6.login(process.env.USER_TOKEN6);
+client7.login(process.env.USER_TOKEN7);
+client8.login(process.env.USER_TOKEN8);
+client9.login(process.env.USER_TOKEN9);
+client10.login(process.env.USER_TOKEN10);
